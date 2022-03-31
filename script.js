14d57793da07208b9efde194bfd8c64e197c2f84
Klemens <klemenskraft@gmail.com>
2022-03-31 20:47:32 +0200

    Add running score, declare winner after 5 rounds

--- script.js
+++ script.js
@@ -24,30 +24,64 @@ function playRound(playerSelection, computerSelection) {
 }
 
 const buttons = document.querySelectorAll('button');
-const div = document.querySelector('div');
+const div1 = document.querySelector('#div1');
+const div2 = document.querySelector('#div2');
+const div3 = document.querySelector('#div3');
 
-for (let button of buttons) {
-  button.addEventListener('click', () => {
-    let computerSelection = computerPlay();
-    let playerSelection;
-    if (button.id === 'btn1') {
-      playerSelection = 'Rock';
-    } else if (button.id === 'btn2') {
-      playerSelection = 'Paper';
-    } else if (button.id === 'btn3') {
-      playerSelection = 'Scissors';
-    }
-    let result = playRound(playerSelection, computerSelection);
-    
-    if (result === 1) {
-      div.textContent = `You win this round. ${playerSelection} beats ${computerSelection}.`;
-    } else if (result === 2) {
-      div.textContent = `You lose this round. ${computerSelection} beats ${playerSelection}`;
-    } else {
-      div.textContent = 'This round is a Draw.';
-    }
-  })
-}
+div1.style.marginTop = '10px';
+div2.style.marginTop = '10px';
+div3.style.cssText = 'margin-top: 20px; font-size: 36px; font-weight: bold;';
+
+let scorePlayer = 0;
+let scoreComputer = 0;
+
+  for (let button of buttons) {
+    button.addEventListener('click', () => {
+      div3.textContent = '';
+      let computerSelection = computerPlay();
+      let playerSelection;
+      if (button.id === 'btn1') {
+        playerSelection = 'Rock';
+      } else if (button.id === 'btn2') {
+        playerSelection = 'Paper';
+      } else if (button.id === 'btn3') {
+        playerSelection = 'Scissors';
+      }
+
+      let result = playRound(playerSelection, computerSelection);
+      
+      if (result === 1) {
+        div1.style.color = 'green';
+        div1.textContent = `You win this round. ${playerSelection} beats ${computerSelection}.`;
+        scorePlayer++;
+        } else if (result === 2) {
+        div1.style.color = 'red';
+        div1.textContent = `You lose this round. ${computerSelection} beats ${playerSelection}`;
+        scoreComputer++;
+      } else {
+        div1.style.color = 'blue';
+        div1.textContent = 'This round is a Draw.';
+      }
+
+      div2.textContent = `Player: ${scorePlayer} | Computer: ${scoreComputer}`
+
+      if (scorePlayer === 5) {
+        div3.style.color = 'green';
+        div3.textContent = 'Player wins this game.'
+        scorePlayer = 0;
+        scoreComputer = 0;
+      } else if (scoreComputer === 5) {
+        div3.style.color = 'red';
+        div3.textContent = 'Computer wins this game.'
+        scorePlayer = 0;
+        scoreComputer = 0;
+      }
+    })
+  }
+
+
+
+
 
 /*
 function game() {
